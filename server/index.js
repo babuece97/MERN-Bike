d53b82cea251be95ea0b706cbdb72be7957d28bb
babuece97 <babuece97@yahoo.com.com>
2022-06-09 02:57:02 +0200

    cliend can send OBJ data to server2

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
-
+//very very important
 app.use(cors());
 app.use(express.json());
 
