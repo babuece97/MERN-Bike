d7ded4bcbffb02c4f31eb1df8ac4445cc41dfd01
babuece97 <babuece97@yahoo.com.com>
2022-06-14 15:07:51 +0200

    UI works

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
-//very very important
+//very very important for connectin Backend
 app.use(cors());
 app.use(express.json());
 
